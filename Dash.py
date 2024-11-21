214ee4b0f9c4a1c2efa772c0e75da3cc5f70556d
João Vitor Souza Ribeiro <jonhvictor121@gmail.com>
2024-11-20 21:52:03 -0300

    Update Dash.py

--- Dash.py
+++ Dash.py
@@ -56,8 +56,8 @@ def export_to_pdf(dataframe, filename="dados_agro.pdf"):
 st.title("Dashboard Interativo: O Agro aplicado")
 
 # Carregar os dados
-file_path = "https://github.com/jonhrib/Agro_2024/blob/main/ATUALIZAÇÕES_Final.xlsx"  # Substitua pelo caminho correto
-data = pd.read_excel(file_path, sheet_name="Página1")
+url = "https://github.com/jonhrib/Agro_2024/blob/main/ATUALIZAÇÕES_Final.xlsx"  # Substitua pelo caminho correto
+data = pd.read_excel(url, sheet_name="Página1")
 
 # Renomear colunas (ajustar conforme necessário)
 columns_mapping = {
